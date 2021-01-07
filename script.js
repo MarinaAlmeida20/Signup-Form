52e35f2dfff2dc12fcd8d2f949ed763af7a33dca
Marina <marinabrito.ma@gmail.com>
2021-01-07 18:07:56 +0000

    1 step

--- script.js
+++ script.js
@@ -2,17 +2,29 @@
 const slidePage = document.querySelector(".slidepage");
 const firtNextBtn = document.querySelector(".nextBtn");
 
-const prevBtnSec = document.querySelector(".prev-1")
-const nextBtnSec = document.querySelector(".next-1")
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
 
-const prevBtnThird = document.querySelector(".prev-2")
-const nextBtnThird = document.querySelector(".next-2")
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
 
-const prevBtnFourth = document.querySelector(".prev-3")
-const submitBtn = document.querySelector(".submit")
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+
+const progressText = document.querySelectorAll(".step p");
+
+const progressCheck = document.querySelectorAll(".step .check");
+
+const bullet = document.querySelectorAll(".step .bullet");
+
+let max = 4;
+
+let current = 1;
 
 firtNextBtn.addEventListener("click", function(){
     slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    current += 1;
 });
 
 nextBtnSec.addEventListener("click", function(){
